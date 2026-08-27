c2a32a9e0442872c149fe5b3d198b52a42e1768b
Prodipta <nayebprodipta@gmail.com>
2026-08-27 22:17:36 +0530

    removed some unwanted line of code

--- Practise_project/src/App.jsx
+++ Practise_project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Background from './components/background';
+//import Background from './components/background';
 import Foreground from './components/foreground';
 function App() 
 {
@@ -10,7 +10,7 @@ function App()
     <>
      <div className="w-screen h-screen relative bg-zinc-800 overflow-hidden">
       
-        <Background />
+        
         <Foreground />
      </div>
     </>
